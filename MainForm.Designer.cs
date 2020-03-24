843f4f342cbc355ae87e943cdb041a60d6a5e500
Kamil Murański <kamil.muranski@bms.com.pl>
2020-03-24 15:55:06 +0100

    Wersja polska

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -42,41 +42,46 @@
             // TaskNumberLabel
             // 
             this.TaskNumberLabel.AutoSize = true;
-            this.TaskNumberLabel.Location = new System.Drawing.Point(39, 30);
+            this.TaskNumberLabel.Location = new System.Drawing.Point(52, 37);
+            this.TaskNumberLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TaskNumberLabel.Name = "TaskNumberLabel";
-            this.TaskNumberLabel.Size = new System.Drawing.Size(69, 13);
+            this.TaskNumberLabel.Size = new System.Drawing.Size(50, 17);
             this.TaskNumberLabel.TabIndex = 0;
-            this.TaskNumberLabel.Text = "Task number";
+            this.TaskNumberLabel.Text = "Numer";
             // 
             // TaskTitleLabel
             // 
             this.TaskTitleLabel.AutoSize = true;
-            this.TaskTitleLabel.Location = new System.Drawing.Point(39, 96);
+            this.TaskTitleLabel.Location = new System.Drawing.Point(52, 118);
+            this.TaskTitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TaskTitleLabel.Name = "TaskTitleLabel";
-            this.TaskTitleLabel.Size = new System.Drawing.Size(50, 13);
+            this.TaskTitleLabel.Size = new System.Drawing.Size(39, 17);
             this.TaskTitleLabel.TabIndex = 1;
-            this.TaskTitleLabel.Text = "Task title";
+            this.TaskTitleLabel.Text = "Tytuł";
             // 
             // TaskNumberTB
             // 
-            this.TaskNumberTB.Location = new System.Drawing.Point(42, 57);
+            this.TaskNumberTB.Location = new System.Drawing.Point(56, 70);
+            this.TaskNumberTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TaskNumberTB.Name = "TaskNumberTB";
-            this.TaskNumberTB.Size = new System.Drawing.Size(715, 20);
+            this.TaskNumberTB.Size = new System.Drawing.Size(952, 22);
             this.TaskNumberTB.TabIndex = 2;
             // 
             // TaskTitleTB
             // 
-            this.TaskTitleTB.Location = new System.Drawing.Point(42, 125);
+            this.TaskTitleTB.Location = new System.Drawing.Point(56, 154);
+            this.TaskTitleTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TaskTitleTB.Name = "TaskTitleTB";
-            this.TaskTitleTB.Size = new System.Drawing.Size(715, 20);
+            this.TaskTitleTB.Size = new System.Drawing.Size(952, 22);
             this.TaskTitleTB.TabIndex = 3;
             // 
             // GenerateGitBranchNameBtn
             // 
             this.GenerateGitBranchNameBtn.Image = global::GitBranchNameGenerator.Properties.Resources.Button;
-            this.GenerateGitBranchNameBtn.Location = new System.Drawing.Point(42, 264);
+            this.GenerateGitBranchNameBtn.Location = new System.Drawing.Point(56, 325);
+            this.GenerateGitBranchNameBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GenerateGitBranchNameBtn.Name = "GenerateGitBranchNameBtn";
-            this.GenerateGitBranchNameBtn.Size = new System.Drawing.Size(57, 54);
+            this.GenerateGitBranchNameBtn.Size = new System.Drawing.Size(76, 66);
             this.GenerateGitBranchNameBtn.TabIndex = 4;
             this.GenerateGitBranchNameBtn.UseVisualStyleBackColor = true;
             this.GenerateGitBranchNameBtn.Click += new System.EventHandler(this.GenerateGitBranchNameBtn_Click);
@@ -84,46 +89,50 @@
             // ResultLabel
             // 
             this.ResultLabel.AutoSize = true;
-            this.ResultLabel.Location = new System.Drawing.Point(42, 355);
+            this.ResultLabel.Location = new System.Drawing.Point(56, 437);
+            this.ResultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ResultLabel.Name = "ResultLabel";
-            this.ResultLabel.Size = new System.Drawing.Size(37, 13);
+            this.ResultLabel.Size = new System.Drawing.Size(46, 17);
             this.ResultLabel.TabIndex = 5;
-            this.ResultLabel.Text = "Result";
+            this.ResultLabel.Text = "Wynik";
             // 
             // ResultTB
             // 
-            this.ResultTB.Location = new System.Drawing.Point(45, 382);
+            this.ResultTB.Location = new System.Drawing.Point(60, 470);
+            this.ResultTB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ResultTB.Name = "ResultTB";
-            this.ResultTB.Size = new System.Drawing.Size(712, 20);
+            this.ResultTB.Size = new System.Drawing.Size(948, 22);
             this.ResultTB.TabIndex = 6;
             // 
             // BugfixRB
             // 
             this.BugfixRB.AutoSize = true;
-            this.BugfixRB.Location = new System.Drawing.Point(42, 169);
+            this.BugfixRB.Location = new System.Drawing.Point(56, 208);
+            this.BugfixRB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BugfixRB.Name = "BugfixRB";
-            this.BugfixRB.Size = new System.Drawing.Size(54, 17);
+            this.BugfixRB.Size = new System.Drawing.Size(57, 21);
             this.BugfixRB.TabIndex = 7;
             this.BugfixRB.TabStop = true;
-            this.BugfixRB.Text = "Bugfix";
+            this.BugfixRB.Text = "Błąd";
             this.BugfixRB.UseVisualStyleBackColor = true;
             // 
             // FeatureRB
             // 
             this.FeatureRB.AutoSize = true;
-            this.FeatureRB.Location = new System.Drawing.Point(42, 206);
+            this.FeatureRB.Location = new System.Drawing.Point(56, 254);
+            this.FeatureRB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.FeatureRB.Name = "FeatureRB";
-            this.FeatureRB.Size = new System.Drawing.Size(61, 17);
+            this.FeatureRB.Size = new System.Drawing.Size(144, 21);
             this.FeatureRB.TabIndex = 8;
             this.FeatureRB.TabStop = true;
-            this.FeatureRB.Text = "Feature";
+            this.FeatureRB.Text = "Wniosek o zmianę";
             this.FeatureRB.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.FeatureRB);
             this.Controls.Add(this.BugfixRB);
             this.Controls.Add(this.ResultTB);
@@ -133,9 +142,10 @@
             this.Controls.Add(this.TaskNumberTB);
             this.Controls.Add(this.TaskTitleLabel);
             this.Controls.Add(this.TaskNumberLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Git branch name generator";
+            this.Text = "Murankowy generator nazwy brancha do Git";
             this.ResumeLayout(false);
             this.PerformLayout();
 
